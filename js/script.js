7c6364467c29e7098a0266eb4321a3b0e93a0c28
ghjks <130291819+ghjks@users.noreply.github.com>
2024-11-18 21:47:14 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.querySelector(".icon-menu").addEventListener("click", (e) => {
+  document.querySelector(".menu__body").classList.toggle("icon");
+  document.querySelector(".icon-menu").classList.toggle("press");
+});
